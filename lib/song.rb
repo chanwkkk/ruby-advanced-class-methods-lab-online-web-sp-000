dc4227652ae022d20049e7141197b39d46cd1260
Yingqi Chen <chanwk18@hotmail.com>
2019-04-07 05:27:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +10,53 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song=self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song=self.new
+    song.name=name
+    song
+  end
+
+  def self.create_by_name(name)
+    song=self.create
+    song.name=name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song|song.name==name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else self.create_by_name(name)
+    end
+end
+
+def self.alphabetical
+  self.all.sort{|x,y|x.name<=>y.name}
+end
+
+def self.new_from_filename(filename)
+  song=self.new
+  name_array=filename.split(/[-\.]/)
+  song.name=name_array[1]
+  song.artist_name=name_array[2]
+  song
+end
+
+
+
+
+
+
+
 
 end
